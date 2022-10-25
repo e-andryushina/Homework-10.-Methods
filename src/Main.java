21ec8286aa5ac180da1d2b287e30cc635e875fa0
e-andryushina <elena-and@inbox.ru>
2022-10-25 05:20:08 +0300

    Задача к курсовой

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,16 @@ public class Main {
 
         int releaseYear = 2022;
         int clientOS = 0;
-        whatIsOSVersion(clientOS,releaseYear);
+        whatIsOSVersion(clientOS, releaseYear);
 
         System.out.println("Задание 3");
         int deliveryDistance = 16;
         howMuchDaysOfDelivery(deliveryDistance);
 
+        System.out.println("Задача к курсовой");
+        int[] number = {23, 56, 78, 53, 47, 89, 144, 345};
+
+        arrayCalculations(number);
     }
 
 
@@ -25,15 +29,14 @@ public class Main {
     public static void isYearLeap(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
-                   }
-        else {
+        } else {
             System.out.println(year + " год не является високосным");
         }
     }
 
     //Урок Методы. Задание 2.
 
-    public static void whatIsOSVersion (int clientOS, int releaseYear) {
+    public static void whatIsOSVersion(int clientOS, int releaseYear) {
         int currentYear = LocalDate.now().getYear();
         if (clientOS == 0 && releaseYear == currentYear) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -49,21 +52,32 @@ public class Main {
     }
 
     //Урок Методы. Задание 3.
-    public  static void howMuchDaysOfDelivery (int deliveryDistance) {
+    public static void howMuchDaysOfDelivery(int deliveryDistance) {
         if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: 1" );
-        }
-        else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
             System.out.println("Потребуется дней: 2");
-        }
-        else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: 3");
-        }
-        else {
+        } else {
             System.out.println("Доставка невозможна");
         }
 
     }
 
 
+    //Задача к курсовой
+    public static void arrayCalculations(int[] arr) {
+        for (int i = 2; i < 7; i++) {
+            if (arr[i] % 2 == 0 || arr[i] == 0) {
+                arr[i]++;
+                System.out.println(arr[i]);
+            } else {
+                System.out.println(arr[i]);
+            }
+        }
+    }
 }
+
+
+
